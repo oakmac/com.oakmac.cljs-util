592a7abb353342a8bc9c8f1de76e2fc9eae3be64
Chris Oakman <chris@oakmac.com>
2016-03-19 23:21:33 -0500

    v1.0.0

---
+++ project.clj
@@ -0,0 +1,11 @@
+(defproject com.oakmac/util "1.0.0"
+
+  :description "A small library of ClojureScript utility functions."
+  :url "https://github.com/oakmac/cljs-util"
+
+  :license {:name "ISC License"
+            :url "https://github.com/oakmac/cljs-util/blob/master/LICENSE.md"
+            :distribution :repo}
+
+  :dependencies [[org.clojure/clojure "1.8.0"]
+                 [org.clojure/clojurescript "1.7.228"]])
---
+++ src-cljs/com/oakmac/util.cljs
@@ -0,0 +1,52 @@
+(ns com.oakmac.util
+  "A collection of utility functions."
+  (:require
+    [goog.dom :as gdom]))
+
+;;------------------------------------------------------------------------------
+;; Logging
+;;------------------------------------------------------------------------------
+
+(defn js-log
+  "Log a JavaScript thing."
+  [js-thing]
+  (js/console.log js-thing))
+
+(defn log
+  "Log a Clojure thing."
+  [clj-thing]
+  (js-log (pr-str clj-thing)))
+
+(defn atom-logger
+  "Log the before / after state of an atom.
+   Designed to be used with the add-watch function."
+  [_kwd _the-atom old-state new-state]
+  (log old-state)
+  (log new-state)
+  (js-log "~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~"))
+
+;;------------------------------------------------------------------------------
+;; DOM Wrappers
+;;------------------------------------------------------------------------------
+
+(defn by-id [el-or-id]
+  (gdom/getElement el-or-id))
+
+(defn remove-el! [el-or-id]
+  (gdom/removeNode (by-id el-or-id)))
+
+;;------------------------------------------------------------------------------
+;; Predicates
+;;------------------------------------------------------------------------------
+
+(defn one? [x]
+  (= x 1))
+
+;;------------------------------------------------------------------------------
+;; Misc
+;;------------------------------------------------------------------------------
+
+(defn half [x]
+  (/ x 2))
+
+(def always-nil (constantly nil))
